18705753ebbf9abf1b07ff73ae45ba6abf39afa2
AlexeyAlexey <alexey.kondratenko@mail.ru>
2015-06-02 22:27:41 +0300

    add format output

--- init.rb
+++ init.rb
@@ -1,3 +1,30 @@
+ActionDispatch::Callbacks.to_prepare do
+  begin
+	action_controller_view_logger ||= Logger.new("#{Rails.root}/log/action_controller_view_logger.log")
+
+	ActiveSupport::Notifications.subscribe "process_action.action_controller" do |*args|
+	  event = ActiveSupport::Notifications::Event.new *args
+	  #event.name      # => "process_action.action_controller"
+	  #event.time      # => start time
+	  #event.end       # => end time
+	  #event.duration  # => 10 (in milliseconds)
+	  #event.transaction_id
+	  #event.payload   # => {:extra=>information}
+	
+	  action_controller_view_logger.info "=>name=#{event.name}<==>transaction_id=#{event.transaction_id}<==>start_time=#{event.time}<==>end_time=#{event.end}<==>duration=#{event.duration}<==>view_runtime=#{event.payload[:view_runtime]}<==>db_runtime=#{event.payload[:db_runtime]}<==>payload=#{event.payload}<="
+	    
+	end
+	  
+	#action_view_logger ||= Logger.new("#{Rails.root}/log/action_view_logger.log")
+
+	ActiveSupport::Notifications.subscribe "render_template.action_view" do |*args|
+	  event = ActiveSupport::Notifications::Event.new *args
+	  action_controller_view_logger.info "=>name=#{event.name}<==>transaction_id=#{event.transaction_id}<==>start_time=#{event.time}<==>end_time=#{event.end}<==>duration=#{event.duration}<==>payload=#{event.payload}<="
+	end
+  rescue Exception => e
+  		
+  end
+end
 Redmine::Plugin.register :redmine_metrics do
   name 'Redmine Metrics plugin'
   author 'Author name'
